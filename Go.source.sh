0bc4c24e5655eb51066ffefd5711e9d3944cebb3
林博仁 <Buo.Ren.Lin@gmail.com>
2017-09-30 01:52:44 +0800

    Add new rules
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

---
+++ Go.source.sh
@@ -0,0 +1,2 @@
+# Use user local prefix as GOPATH prefix(for install Go packages)
+export GOPATH="${HOME}/.local" 
---
+++ ods2sql.source.sh
@@ -0,0 +1,1 @@
+install_executable_search_path "$HOME/軟體/ods2sql - A Python script to convert an ODS file into SQL"
