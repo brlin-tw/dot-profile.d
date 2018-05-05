51ef58846ba76a95d3d3f039a2c9e48464b500ac
林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>
2018-05-05 09:47:44 +0800

    Go: Add custom installation
    Signed-off-by: 林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>

--- Go.source.sh
+++ Go.source.sh
@@ -1,2 +1,4 @@
 # Use user local prefix as GOPATH prefix(for install Go packages)
 export GOPATH="${HOME}/.local" 
+export GOROOT="${HOME}/軟體/go1.10.2.linux-amd64"
+install_executable_search_path "${GOROOT}/bin"
