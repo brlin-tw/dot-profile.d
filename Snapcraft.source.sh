e9a378d35708144f4e95a9722b7a033d02810985
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2018-11-09 15:47:58 +0800

    Add snapcraft setup
    Signed-off-by: 林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>

---
+++ Snapcraft.source.sh
@@ -0,0 +1,17 @@
+# shellcheck shell=bash
+# shellcheck disable=SC2034
+
+# Snapcraft-specific settings
+# 林博仁(Buo-ren, Lin) © 2018
+
+## Include Guard to prevent multiple sourcing
+if [ -n "${_PROFILE_D_INC_GRD_SNAPCRAFT}" ]; then
+	return 0
+fi
+
+# Use persistent LXD container for snap building
+SNAPCRAFT_BUILD_ENVIRONMENT=lxd
+export SNAPCRAFT_BUILD_ENVIRONMENT
+
+## Set Include Guard
+declare _PROFILE_D_INC_GRD_SNAPCRAFT=1
